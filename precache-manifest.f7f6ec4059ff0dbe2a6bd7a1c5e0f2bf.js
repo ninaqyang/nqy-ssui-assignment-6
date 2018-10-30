8ca3d135568e68b9ff6b1efe8c3071f9f0d13f70
Nina Yang <ninaqy13@gmail.com>
2018-10-30 12:45:09 -0400

    Updates

---
+++ precache-manifest.f7f6ec4059ff0dbe2a6bd7a1c5e0f2bf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e0256edf5436811a2d91ac1e35aa856",
+    "url": "/favorite-places/static/media/fish_cheeks.2e0256ed.jpg"
+  },
+  {
+    "revision": "6dc16731332384aafecb",
+    "url": "/favorite-places/static/css/main.30ca82fa.chunk.css"
+  },
+  {
+    "revision": "53b84dcffe523d3dd20b",
+    "url": "/favorite-places/static/js/1.53b84dcf.chunk.js"
+  },
+  {
+    "revision": "f207ca0cfc07fa2f6567",
+    "url": "/favorite-places/static/js/runtime~main.f207ca0c.js"
+  },
+  {
+    "revision": "70fde55f48ac8c320dd7d40d5fad40b2",
+    "url": "/favorite-places/static/media/stamp.70fde55f.png"
+  },
+  {
+    "revision": "08a39973cc9f35d9160892fa36c72382",
+    "url": "/favorite-places/static/media/boba_guys.08a39973.jpg"
+  },
+  {
+    "revision": "6dc16731332384aafecb",
+    "url": "/favorite-places/static/js/main.6dc16731.chunk.js"
+  },
+  {
+    "revision": "af227069979a64994fb18e6d139f609a",
+    "url": "/favorite-places/static/media/mountain_house.af227069.jpg"
+  },
+  {
+    "revision": "fc9db3dd6974ee30326117319276e7ef",
+    "url": "/favorite-places/static/media/nom_wah.fc9db3dd.jpg"
+  },
+  {
+    "revision": "e1be16a7c833587f201b51b8eb02570b",
+    "url": "/favorite-places/static/media/ramen_lab.e1be16a7.jpg"
+  },
+  {
+    "revision": "44975d3e5b576dd773a0ecad3294f3bc",
+    "url": "/favorite-places/static/media/spice_world.44975d3e.jpg"
+  },
+  {
+    "revision": "ba7f9e59ff321fa69e90ac62fb1a4c99",
+    "url": "/favorite-places/index.html"
+  }
+];
